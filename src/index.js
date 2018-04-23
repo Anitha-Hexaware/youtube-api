f655c42aba3d1f4224ac976ac7c81b8d8d316f3b
Anitha-Hexaware <38136.hexaware@gmail.com>
2018-04-23 11:29:23 +0530

    youtube video came

--- src/index.js
+++ src/index.js
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, {
+    Component
+} from 'react';
 import ReactDOM from 'react-DOM';
 import SearchBar from './Component/search_bar';
 import youAPISearch from 'youtube-api-search';
+import VideoList from './Component/video-list';
+import VideoDetail from './Component/video-detail';
+// import VideoListItem from './Component/video-list-item';
 
 const ApiKey = 'AIzaSyBmanQBjPUDOS6uMPmCnuprEWBN-2N4TXE';
 
-youAPISearch({key: ApiKey, term: 'surfboards'}, function(data){
-console.log(data);
-});
-
 // create a new component. This component should produce some HTML
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos : [],
+            selectedVideo: null
+        };
 
-const App = () => {
-        return ( <div>
+
+        youAPISearch({
+            key: ApiKey,
+            term: 'blockchains'
+        }, (videos) =>  {
+            // this.setState({videos : videos});
+            this.setState({
+                videos : videos,
+                selectedVideo : videos[0]
+            });
+            console.log(videos);
+
+        });
+    }
+
+
+    render() {
+        return ( 
+        <div>
             <SearchBar/>
-            </div>);
+            <VideoDetail video ={this.state.selectedVideo} />
+            <VideoList videos = {this.state.videos}/>
+            </div>
+            );
         }
-        // Take this component's generated HTML and put it on the page (in the DOM).
-        ReactDOM.render( < App / > , document.querySelector('.container'));
+    }
+    // Take this component's generated HTML and put it on the page (in the DOM).
+    ReactDOM.render( <App/> , document.querySelector('.container'));
